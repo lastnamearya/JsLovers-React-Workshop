ff41cfe5e52a11ec173e1579e97a5cae617fd1f4
lastnamearya <arya.jigyasu6815@gmail.com>
2018-06-10 12:23:57 +0530

    SASS added in our code to make CSS Changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './sass/style.css';
 import Foo from './Foo.js';
 
 class App extends Component {
