41a495297b0b4591d36e60266e56cc690444612d
lastnamearya <arya.jigyasu6815@gmail.com>
2018-06-10 11:40:39 +0530

    Hello World with a Stateless Componenet

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Foo from './Foo.js';
 
 class App extends Component {
   render() {
@@ -8,8 +9,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Js Lovers</h1>
         </header>
+        <Foo />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
